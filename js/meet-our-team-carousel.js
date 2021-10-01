8fac989abc3d066a436f0e570cea4e8ff07470dd
Serhiy Pop <sergey.pope@gmail.com>
2021-10-01 17:19:39 +0300

    fixed carousel on <meet our team> section, foxed css to it, removed *margin= auto

--- js/meet-our-team-carousel.js
+++ js/meet-our-team-carousel.js
@@ -1,10 +1,89 @@
 
 (function () {
+
+
+
     const slides = [
-        '<img src="img/meet-our-teem-carusel-block-pics/helena-farse-pic.webp" alt="Helena Farse foto">',
-        '<img src="img/meet-our-teem-carusel-block-pics/dan-pitts-pic.webp" alt="Dan Pitts foto">',
-        '<img src="img/meet-our-teem-carusel-block-pics/tomas-willens-pic.webp" alt="Tomas Willens foto">',
-        '<img src="img/meet-our-teem-carusel-block-pics/lucy-madins-pic.webp" alt="Lucy Madins foto">'
+        `<div class="team-members-carousel-item-generic">
+            <img src="img/meet-our-teem-carusel-block-pics/helena-farse-pic.webp" alt="Helena Farse foto">
+            <h2 class="team-members-carousel-h2-style">Helena Farse</h2>
+            <div class="team-members-about">
+                <p class="team-members-about">Professional guide | 10 years of experience</p>
+            </div>
+            <div class="team-members-social-icons-container">
+                <ul class="team-members-social-icons-list">
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://facebook.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/facebook-icon.svg" alt="Facebook"></a></p>
+                    </li>
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://instagram.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/instagram-icon.svg" alt="Instagram"></a></p>
+                    </li>
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://youtube.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/youtube-icon.svg" alt="YouTube"></a></p>
+                    </li>
+                </ul>
+            </div>
+        </div>`,
+        `<div class="team-members-carousel-item-generic">
+            <img src="img/meet-our-teem-carusel-block-pics/dan-pitts-pic.webp" alt="Dan Pitts foto">
+            <h2 class="team-members-carousel-h2-style">Dan Pitts</h2>
+            <div class="team-members-about">
+                <p class="team-members-about">Professional guide | 5 years of experience</p>
+            </div>
+            <div class="team-members-social-icons-container">
+                <ul class="team-members-social-icons-list">
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://facebook.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/facebook-icon.svg" alt="Facebook"></a></p>
+                    </li>
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://instagram.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/instagram-icon.svg" alt="Instagram"></a></p>
+                    </li>
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://youtube.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/youtube-icon.svg" alt="YouTube"></a></p>
+                    </li>
+                </ul>
+            </div>
+        </div>`,
+        `<div class="team-members-carousel-item-generic">
+            <img src="img/meet-our-teem-carusel-block-pics/tomas-willens-pic.webp" alt="Tomas Willens foto">
+            <h2 class="team-members-carousel-h2-style">Tomas Willens</h2>
+            <div class="team-members-about">
+                <p class="team-members-about">Photographer | 5 years of experience</p>
+            </div>
+            <div class="team-members-social-icons-container">
+                <ul class="team-members-social-icons-list">
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://facebook.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/facebook-icon.svg" alt="Facebook"></a></p>
+                    </li>
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://instagram.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/instagram-icon.svg" alt="Instagram"></a></p>
+                    </li>
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://youtube.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/youtube-icon.svg" alt="YouTube"></a></p>
+                    </li>
+                </ul>
+            </div>
+        </div>`,
+        `<div class="team-members-carousel-item-generic">
+            <img src="img/meet-our-teem-carusel-block-pics/lucy-madins-pic.webp" alt="Lucy Madins foto">
+            <h2 class="team-members-carousel-h2-style">Lucy Madins</h2>
+            <div class="team-members-about">
+                <p class="team-members-about">Photographer | 5 years of experience</p>                                
+            </div>
+            <div class="team-members-social-icons-container">
+                <ul class="team-members-social-icons-list">
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://facebook.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/facebook-icon.svg" alt="Facebook"></a></p>
+                    </li>
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://instagram.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/instagram-icon.svg" alt="Instagram"></a></p>
+                    </li>
+                    <li class="team-members-social-icons-element">
+                        <p><a target="_blank" href="https://youtube.com/"><img id="team-members-social-icons-element" src="img/img-social-icons/youtube-icon.svg" alt="YouTube"></a></p>
+                    </li>
+                </ul>
+            </div>
+        </div>`
     ]
 
     let currentSlide = 0;
