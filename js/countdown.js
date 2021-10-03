1911ea71285a87742d7681da84a6c7d949ab0158
ivanchornyi88 <i.chornyi.88@gmail.com>
2021-10-03 08:49:42 +0300

    added animation to countdown

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,35 @@
+
+//Animation Text
+
+const text = document.querySelector('.animation');
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+for(let i=0; i < splitText.length; i++){
+  text.innerHTML += "<span>"+ splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 100);
+
+function onTick(){
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++
+  if(char === splitText.length){
+    complete();
+    return;
+  }
+}
+function complete() {
+clearInterval(timer);
+timer = null;
+}
+
+//Countdown
+
 const countdown = () => {
-    const countDate = new Date("2022 Jan 01, 00:00:00").getTime();
+    const countDate = new Date("2021 Dec 01, 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
   
