a21c505e3584a32fd4b4d66183e36827aa289a1c
afanasieva-anastasiia <afanasieva.dev@gmail.com>
2021-10-05 15:54:57 +0300

    changed carousel's css for our tours

--- js/our-tours-carousel.js
+++ js/our-tours-carousel.js
@@ -2,35 +2,51 @@
   const ourToursSlides = [
     `<article class="our-tours-carousel-item">
         <img alt="Illustration" src="img/img-our-tours/tanzania.jfif">
-        <h3>5 Days in Tanzania</h3>
+        <div><h3>5 Days in Tanzania</h3></div>
+        <div>
         <p class="our-tours-description">From Serengeti to Ngorongoro, with nights in comfortable hotels and breakfasts included.</p>
+        </div>
+        <div>
         <p class="our-tours-bold">Departing: 9 Nov 2019</p>
         <p class="our-tours-bold-yellow">Cost: $4000</p>
         <a href="#">Learn more</a>
+        </div>
     </article>`,
     `<article class="our-tours-carousel-item">
         <img alt="Illustration" src="img/img-our-tours/kenya.jfif">
-        <h3>Amazing Week in Kenya</h3>
+        <div><h3>Amazing Week in Kenya</h3></div>
+        <div>
         <p class="our-tours-description">Starting from Nairobi, we'll visit Maasai Mara, Lake Nakuru, Lake Naivasha andHell’s Gate.</p>
+        </div>
+        <div>
         <p class="our-tours-bold">Departing: 12 Jan 2020</p>
         <p class="our-tours-bold-yellow">Cost: $5500</p>
         <a href="#">Learn more</a>
+        </div>
     </article>`,
     `<article class="our-tours-carousel-item">
         <img alt="Illustration" src="img/img-our-tours/amboseli-park.jfif">
-        <h3>Amboseli Park in Kenya</h3>
+        <div><h3>Amboseli Park in Kenya</h3></div>
+        <div>
         <p class="our-tours-description">Go on a 5-days amazing tour around the Amboseli Park in Kenya</p>
+        </div>
+        <div>
         <p class="our-tours-bold">Departing: 03 Mar 2020</p>
         <p class="our-tours-bold-yellow">Cost: $6200</p>
         <a href="#">Learn more</a>
+        </div>
     </article>`,
     `<article class="our-tours-carousel-item">
         <img alt="Illustration" src="img/img-our-tours/jozani-chwaka-bay.jfif">
-        <h3>Jozani Chwaka Bay National Park</h3>
+        <div><h3>Jozani Chwaka Bay National Park</h3></div>
+        <div>
         <p class="our-tours-description">Visit Josani, or as it is also called Fairytale Forest, the only national park in Zanzibar.</p>
+        </div>
+        <div>
         <p class="our-tours-bold">Departing: 03 Mar 2020</p>
         <p class="our-tours-bold-yellow">Cost: $6200</p>
         <a href="#">Learn more</a>
+        </div>
     </article>`,
   ];
 
